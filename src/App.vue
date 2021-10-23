0584001600c5b3df96758a5a52ad53d841630862
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-23 18:09:43 -0500

    se agregaron fundamentos basicos de VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <Counter title="Entrada" />
+  <Counter :start="10" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,68 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+  <div>
+    <button v-on:click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 100,
+      validator(value) {
+        return value >= 0;
+      },
+      //   required: true,
+    },
+  },
+  //   name: "Prueba",
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter;
+    },
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter() {
+      console.log("se ejecuta computed");
+      return this.counter * this.counter;
+    },
+    customTitle() {
+      return this.title ? this.title : "Counter";
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
