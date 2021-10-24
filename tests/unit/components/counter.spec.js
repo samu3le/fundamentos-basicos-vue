813b2099105b1143eca3d9d24fc733b0dda5fc6a
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-24 11:52:32 -0500

    update test Counter

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,12 +2,15 @@ import { shallowMount, mount } from "@vue/test-utils";
 import Counter from "@/components/Counter.vue";
 
 describe("Counter Component", () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(Counter);
+    });
     // test("Debe de hacer match con el snapshot", () => {
     //     const wrapper = shallowMount(Counter);
     //     expect(wrapper.html()).toMatchSnapshot();
     // });
     test('H2 Debe tener el valor por defecto "Counter"', () => {
-        const wrapper = shallowMount(Counter);
         expect(wrapper.find("h2").exists()).toBeTruthy();
 
         const h2Value = wrapper.find("h2").text();
@@ -15,23 +18,38 @@ describe("Counter Component", () => {
     });
 
     test("El valor por defecto debe de ser 100 en el p", () => {
-        const wrapper = shallowMount(Counter);
         // const value = wrapper.findAll("p");
         const value = wrapper.find('[data-testid="counter"]').text();
         expect(value).toBe("100");
     });
 
     test("debe de incrementar y decrementar el contador", async() => {
-        const wrapper = shallowMount(Counter);
-        const increaseBtn = wrapper.find("button");
+        const [increaseBtn, decreaseBtn] = wrapper.findAll("button");
         await increaseBtn.trigger("click");
-        let value = wrapper.find('[data-testid="counter"]').text();
-        expect(value).toBe("101");
+        await increaseBtn.trigger("click");
+        await increaseBtn.trigger("click");
+        await decreaseBtn.trigger("click");
+        await decreaseBtn.trigger("click");
 
-        const decreaseBtn = wrapper.findAll("button")[1];
-        await decreaseBtn.trigger("click");
-        await decreaseBtn.trigger("click");
-        value = wrapper.find('[data-testid="counter"]').text();
-        expect(value).toBe("99");
+        const value = wrapper.find('[data-testid="counter"]').text();
+        expect(value).toBe("101");
+    });
+
+    test("debe establecer el valor por defecto ", () => {
+        // console.log(wrapper.props());
+        // const {start} = wrapper.props()
+        const start = wrapper.props("start");
+        console.log(start);
+    });
+
+    test("should show prop title", () => {
+        const title = "Hola Mundo";
+        const wrapper = shallowMount(Counter, {
+            props: {
+                title,
+            },
+        });
+
+        expect(wrapper.find("h2").text()).toBe(title);
     });
 });
